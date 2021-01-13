c9598393b910b71af312635adb2550db7b759d0a
Jayanaka <jayanakawka@gmail.com>
2021-01-13 17:40:24 +0530

    setup socket.io

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const path = require('path')
+const http = require('http')
+const socketIO = require('socket.io')
 
 const app = express()
+
+const server = http.createServer(app)
+
+const io = socketIO(server)
+
+io.on('connection',socket => {
+    io.emit('message','Hello')
+})
 
 app.use(express.static(path.join(__dirname,'public')))
 
 const PORT = 6500 || process.env.PORT
 
-app.listen(PORT,() => console.log(`Server is listning at port ${PORT} ....`))
+server.listen(PORT,() => console.log(`Server is listning at port ${PORT} ....`))
